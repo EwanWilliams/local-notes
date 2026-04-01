0faf2b2f0b63eae89cf03304d5e02ca00284354d
Ewan Williams <ewan@ewan.name>
2026-04-01 20:05:18 +0100

    Basic front end quill editor set up

--- src/client/components/Editor.jsx
+++ src/client/components/Editor.jsx
@@ -4,6 +4,27 @@ import 'quill/dist/quill.snow.css';
 
 
 export function Editor() {
+
+    // custom toolbar layout and options
+    const toolbarConfig = [
+        // heading/normal dropdown
+        [{ 'header': [1, 2, 3, 4, false] }],
+        // font options
+        [{ 'font': [] }],
+        // button options
+        ['bold', 'italic', 'underline', 'strike', 'code-block'],
+        // alignment options
+        [{ 'align': [] }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],
+        // list options
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+        // hyperlink option
+        ['link'],
+        // colour options
+        [{ 'color': [] }, { 'background': [] }],
+        ['clean']
+    ]
+
     const containerRef = useCallback(wrapper => {
         // make sure wrapper exists and is reset on reinitialise
         if (wrapper == null) return;
@@ -12,10 +33,10 @@ export function Editor() {
         const editor = document.createElement('div');
         wrapper.append(editor);
         // initialise quill instance
-        new Quill(editor, { theme: 'snow'});
+        new Quill(editor, { theme: 'snow', modules: { toolbar: toolbarConfig }});
     }, []);
 
     return (
-        <div id='editorContainer' ref={containerRef}></div>
+        <div className='editorContainer' ref={containerRef}></div>
     );
 }
