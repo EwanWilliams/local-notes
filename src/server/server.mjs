0f8177486a5af937c471e43551951a8472931143
Ewan Williams <ewan@ewan.name>
2026-04-02 14:41:40 +0100

    Socket.io admin panel set up

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -5,11 +5,14 @@ import { createServer } from 'http';
 import morgan from 'morgan';
 import cors from 'cors';
 import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 
 
 // access env variables
 const PORT = process.env._PORT;
 const CLIENT = process.env._CLIENT_PORT;
+const ADMIN_USER = process.env.SOCKET_ADMIN_USER;
+const ADMIN_PWD = process.env.SOCKET_ADMIN_PWD;
 
 
 // initialise express and socket server
@@ -17,7 +20,8 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: [`http://localhost:${CLIENT}`],
+        origin: [`http://localhost:${CLIENT}`, 'https://admin.socket.io'],
+        credentials: true,
         methods: ['GET', 'POST']
     }
 });
@@ -39,10 +43,18 @@ httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // SOCKET.IO FUNCTIONALITY
 io.on('connection', (socket) => {
-    console.log(socket.id);
-
     // on receiving changes made by a user
     socket.on('user-changes', delta => {
         socket.broadcast.emit('new-changes', delta);
     });
+});
+
+
+instrument(io, {
+    auth: {
+        type: 'basic',
+        username: ADMIN_USER,
+        password: ADMIN_PWD
+    },
+    mode: 'development',
 });
