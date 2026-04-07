8d1f71c1853eb95f34231313d15be5a394c0d324
Ewan Williams <ewan@ewan.name>
2026-04-07 17:38:08 +0100

    comments

--- src/client/pages/components/FileBrowser.jsx
+++ src/client/pages/components/FileBrowser.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 export default function FileBrowser({files = []}) {
     const navigate = useNavigate();
 
+    // produce list of file elements
     const fileList = files.map(file => (
         <li key={file._id}>
             {file.title}
@@ -11,6 +12,7 @@ export default function FileBrowser({files = []}) {
        </li>
     ));
 
+    // return list of files within container
     return (
         <div id='file-browser' className='file-browser'>
             {fileList.length > 0 ? <ul>{fileList}</ul> : <p>No files.</p>}
